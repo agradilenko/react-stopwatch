e40e084f61823473974cbd38c8bbdf26ab7b20d3
agradilenko <44676263+agradilenko@users.noreply.github.com>
2020-06-25 22:23:26 +0300

    Updates

---
+++ precache-manifest.d8fae22b33233f6026b3013f2c18580c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7ecc10ddbe5fd778645cfd046b4421",
+    "url": "/react-stopwatch/index.html"
+  },
+  {
+    "revision": "920beb3c455a05f86450",
+    "url": "/react-stopwatch/static/css/main.1c02c3bd.chunk.css"
+  },
+  {
+    "revision": "2c73884927e117484339",
+    "url": "/react-stopwatch/static/js/2.529acee7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-stopwatch/static/js/2.529acee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920beb3c455a05f86450",
+    "url": "/react-stopwatch/static/js/main.db9f67ea.chunk.js"
+  },
+  {
+    "revision": "630006571d42a91f0bac",
+    "url": "/react-stopwatch/static/js/runtime-main.35250e60.js"
+  }
+]);
